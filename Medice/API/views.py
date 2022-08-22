fb2e30f1c476da79b30f5c4887c05d6884a2ed50
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 14:16:20 -0300

    Criação da View de Pacientes

--- Medice/API/views.py
+++ Medice/API/views.py
@@ -1,18 +1,33 @@
+from ast import Return
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import usuario
-from .models import Usuarios
+from .serializers import usuario,paciente
+from .models import Usuarios, Pacientes
 
 
 #Criação do views de Usuarios 
 class User(APIView):
     #def get (self,request):
     def post (self, request):
-        serializer = usuario(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response ({"status": "success", "data": serializer.data}, status = status.HTTP_200_OK)
+        serializers = usuario(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response ({"status": "success", "data": serializers.data}, status = status.HTTP_200_OK)
         else:
-                return Response({"status": "error", "data": serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+            return Response({"status": "error", "data": serializers.errors}, status = status.HTTP_400_BAD_REQUEST)
+
+
+# Criação do views de Pacientes
+
+class Patient(APIView):
+    def post(self, request):
+        serializers = paciente(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response({"status": "success", "data": serializers.data}, status = status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializers.errors}, status = status.HTTP_400_BAD_REQUEST)
+
+
 
