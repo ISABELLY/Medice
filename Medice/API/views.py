9b90ded70650ebc479969a3e8d0e4baaa778b2b4
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 10:25:53 -0300

    Criação da View de Usuarios

--- Medice/API/views.py
+++ Medice/API/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from turtle import home
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import usuario
+from .models import Usuarios
 
-# Create your views here.
+
+#Criação do views de Usuarios 
+class Usuario(APIView):
+    def get (self,request):
+
+    def post (self, request):
+        serializer = usuario(data = request.data)
+        if serializer.is_valid():
+            return Response ({"status": "success", "data": serializer.data}, status = status.HTTP_200_OK)
+        else:
+                return Response({"status": "error", "data": serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+                
