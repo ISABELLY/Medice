e69f751f30cbee076e46d6d05fbb11ef27d45160
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 14:17:57 -0300

    Criação da urls de Paciente

--- Medice/API/urls.py
+++ Medice/API/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import  User
+from .views import  User,Patient
 
 urlpatterns = [
     path('Usuario/', User.as_view()),
+    path('Paciente/', Patient.as_view()),
   
 ]
