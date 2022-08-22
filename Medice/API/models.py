0ccf0bf90de6e0a3b0bf5f8e27d1b47c9494e2fe
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 12:18:18 -0300

    Criação Models Pacientes

--- Medice/API/models.py
+++ Medice/API/models.py
@@ -5,7 +5,15 @@ from django.db import models
 #Criação do models de Usuarios
 class Usuarios(models.Model):
     id = models.AutoField(primary_key=True)
-    login = models.CharField(max_length=200)
-    senha = models.FloatField(max_length=30)
+    login = models.CharField(max_length=200, null=False)
+    senha = models.FloatField(max_length=30, null=False)
     nome = models.CharField(max_length=30)
     ativo = models.BooleanField(null=False, default=True)
+
+#Criação do models de Pacientes
+class Pacientes(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=30, null=False) 
+    idade = models.IntegerField(max_length=2)
+    data_nascimento = models.DateField(null=True)
+    ativo = models.BooleanField(null=False, default=True)  
