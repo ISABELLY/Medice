4294ef7ded213f5f8197928b94f65edce68905cc
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 12:11:27 -0300

    Atualizações

--- Medice/API/views.py
+++ Medice/API/views.py
@@ -1,4 +1,3 @@
-from turtle import home
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,11 +7,11 @@ from .models import Usuarios
 
 #Criação do views de Usuarios 
 class User(APIView):
-    def get (self,request):
-
-     def post (self, request):
+    #def get (self,request):
+    def post (self, request):
         serializer = usuario(data = request.data)
         if serializer.is_valid():
+            serializer.save()
             return Response ({"status": "success", "data": serializer.data}, status = status.HTTP_200_OK)
         else:
                 return Response({"status": "error", "data": serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
