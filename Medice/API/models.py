0f2ac85ae445a65ad7efabf4e8d6625491535649
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 09:49:18 -0300

    Criação do Models Usuarios

--- Medice/API/models.py
+++ Medice/API/models.py
@@ -1,3 +1,11 @@
+from email.errors import InvalidDateDefect
+from mailbox import NotEmptyError
 from django.db import models
 
-# Create your models here.
+#Criação do models de Usuarios
+class Usuarios(models.Model):
+    id = models.AutoField(primary_key=True)
+    login = models.CharField(max_length=200)
+    senha = models.FloatField(max_length=30)
+    nome = models.CharField(max_length=30)
+    ativo = models.BooleanField(null=False, default=True)
