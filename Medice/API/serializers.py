8e566b7e59ee9c2782dd7192c1b8c61b9a44cde5
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 12:22:03 -0300

    Criação do views de Pacientes

--- Medice/API/serializers.py
+++ Medice/API/serializers.py
@@ -2,7 +2,7 @@ import logging
 from mailbox import NotEmptyError
 from pyexpat import model
 from rest_framework import serializers
-from .models import Usuarios
+from .models import Pacientes, Usuarios
 
 
 #Criação do serializers de Usuarios
@@ -10,3 +10,10 @@ class usuario(serializers.ModelSerializer):
     class Meta:
         model = Usuarios
         fields = ('__all__')
+
+ #Criação do serializers de Pacientes
+
+class paciente(serializers.ModelSerializer):
+    class Meta:
+        model = Pacientes
+        fiels = ('__all__')
