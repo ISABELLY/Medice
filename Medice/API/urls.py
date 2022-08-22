040fe447aa407aa1b28508bb355d12f4513750fa
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-08-22 14:19:38 -0300

    atualização

--- Medice/API/urls.py
+++ Medice/API/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import  User,Patient
 
 urlpatterns = [
-    path('Usuario/', User.as_view()),
-    path('Paciente/', Patient.as_view()),
+    path('usuario/', User.as_view()),
+    path('paciente/', Patient.as_view()),
   
 ]
