e510b1d0af70d8c79a327f6e7a39236025f08390
Isabelly.Gomes <isabellygomes58@gmail.com>
2022-09-02 14:49:40 -0300

    Implementado o metodo DELETE

--- Medice/pacientes/views.py
+++ Medice/pacientes/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
+from django.shortcuts import get_object_or_404
 
 
 from .serializers import PacienteSerializer
@@ -11,3 +12,8 @@ class PacienteViewSet(ModelViewSet):
     serializer_class = PacienteSerializer
     queryset = Pacientes.objects.all()
     pagination_class = LimitOffsetPagination
+
+    def delete(self, request, id=None):
+        paciente = get_object_or_404(Pacientes, id=id)
+        paciente.delet()
+        return Response({"status": "success", "data": "Item Deleted"})
